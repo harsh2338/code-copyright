56abb117966dd19dc56bac90e6cf09302fd6d5fa
harsh2338 <harsh.aayush.r1@gmail.com>
2021-12-15 21:56:04 +0530

    Added smart contract for uploading code

--- Javascript/index.js
+++ Javascript/index.js
@@ -15,126 +15,137 @@ import { getHashSet } from "./hashSet.js";
 import { getNGrams } from "./nGrams.js";
 import { getCodeFingerprint } from "./codeFingerprint.js";
 class Visitor {
-	languageParsers = [Java8Parser, Python3Parser, CPP14Parser, JavaScriptParser];
-	constructor(languageIndex) {
-		this.languageIndex = languageIndex;
-		this.listOfRuleNames = [];
-	}
+  languageParsers = [Java8Parser, Python3Parser, CPP14Parser, JavaScriptParser];
+  constructor(languageIndex) {
+    this.languageIndex = languageIndex;
+    this.listOfRuleNamesIndices = [];
+    this.listOfRuleNames = [];
+  }
 
-	getRuleNames() {
-		return this.listOfRuleNames;
-	}
-	visitChildren(ctx) {
-		if (!ctx) {
-			return;
-		}
-		if (ctx.children) {
-			let toIgnore =
-				ctx.children.length >= 1 &&
-				ctx.children[0] instanceof antlr4.ParserRuleContext;
+  getRuleNames() {
+    return this.listOfRuleNames;
+  }
+  getRuleNamesIndices() {
+    return this.listOfRuleNamesIndices;
+  }
+  visitChildren(ctx) {
+    if (!ctx) {
+      return;
+    }
+    if (ctx.children) {
+      let toIgnore =
+        ctx.children.length >= 1 &&
+        ctx.children[0] instanceof antlr4.ParserRuleContext;
 
-			if (!toIgnore) {
-				let ruleName =
-					this.languageParsers[this.languageIndex].ruleNames[ctx.ruleIndex];
-				console.log(ruleName + " -> " + ctx.getText());
-				this.listOfRuleNames.push(ctx.ruleIndex);
-			}
+      if (!toIgnore) {
+        let ruleName =
+          this.languageParsers[this.languageIndex].ruleNames[ctx.ruleIndex];
+        console.log(ruleName + " -> " + ctx.getText());
+        this.listOfRuleNames.push(ruleName);
+        this.listOfRuleNamesIndices.push(ctx.ruleIndex);
+      }
 
-			return ctx.children.map((child) => {
-				if (child.children && child.children.length != 0) {
-					return child.accept(this);
-				} else {
-					return child.getText();
-				}
-			});
-		}
-	}
+      return ctx.children.map((child) => {
+        if (child.children && child.children.length != 0) {
+          return child.accept(this);
+        } else {
+          return child.getText();
+        }
+      });
+    }
+  }
 }
 
 const sourceCodeFileNames = [
-	"java_source_code.java",
-	"python_source_code.py",
-	"cpp_source_code.cpp",
-	"js_source_code.js",
+  "java_source_code.java",
+  "python_source_code.py",
+  "cpp_source_code.cpp",
+  "js_source_code.js",
 ];
 
 function getJavaParser(chars) {
-	const lexer = new Java8Lexer(chars);
-	const tokens = new antlr4.CommonTokenStream(lexer);
-	const parser = new Java8Parser(tokens);
-	return parser;
+  const lexer = new Java8Lexer(chars);
+  const tokens = new antlr4.CommonTokenStream(lexer);
+  const parser = new Java8Parser(tokens);
+  return parser;
 }
 
 function getJavaScriptParser(chars) {
-	const lexer = new JavaScriptLexer(chars);
-	const tokens = new antlr4.CommonTokenStream(lexer);
-	const parser = new JavaScriptParser(tokens);
-	return parser;
+  const lexer = new JavaScriptLexer(chars);
+  const tokens = new antlr4.CommonTokenStream(lexer);
+  const parser = new JavaScriptParser(tokens);
+  return parser;
 }
 
 function getPythonParser(chars) {
-	const lexer = new Python3Lexer(chars);
-	const tokens = new antlr4.CommonTokenStream(lexer);
-	const parser = new Python3Parser(tokens);
-	return parser;
+  const lexer = new Python3Lexer(chars);
+  const tokens = new antlr4.CommonTokenStream(lexer);
+  const parser = new Python3Parser(tokens);
+  return parser;
 }
 
 function getCPPParser(chars) {
-	console.log("here");
-	const lexer = new CPP14Lexer(chars);
-	const tokens = new antlr4.CommonTokenStream(lexer);
-	const parser = new CPP14Parser(tokens);
-	return parser;
+  console.log("here");
+  const lexer = new CPP14Lexer(chars);
+  const tokens = new antlr4.CommonTokenStream(lexer);
+  const parser = new CPP14Parser(tokens);
+  return parser;
 }
 
-function printTree(languageIndex) {
-	var fname = sourceCodeFileNames[languageIndex];
-	const input = fs.readFileSync(`./source_code/${fname}`, "utf8");
-	const chars = new antlr4.InputStream(input);
-	var parser;
-	var tree;
-	switch (languageIndex) {
-		case 0:
-			parser = getJavaParser(chars);
-			parser.buildParseTrees = true;
-			tree = parser.classDeclaration();
-			break;
-		case 1:
-			parser = getPythonParser(chars);
-			parser.buildParseTrees = true;
-			tree = parser.compound_stmt();
-			break;
-		case 2:
-			parser = getCPPParser(chars);
-			parser.buildParseTrees = true;
-			tree = parser.compoundStatement();
-			break;
-		case 3:
-			parser = getJavaScriptParser(chars);
-			parser.buildParseTrees = true;
-			tree = parser.program();
-	}
+function printTree(languageIndex, fileName) {
+  //   var fname = sourceCodeFileNames[languageIndex];
+  //   const input = fs.readFileSync(`./source_code/${fname}`, "utf8");
+  const input = fs.readFileSync(fileName, "utf8");
+  const chars = new antlr4.InputStream(input);
+  var parser;
+  var tree;
+  switch (languageIndex) {
+    case 0:
+      parser = getJavaParser(chars);
+      parser.buildParseTrees = true;
+      tree = parser.classDeclaration();
+      break;
+    case 1:
+      parser = getPythonParser(chars);
+      parser.buildParseTrees = true;
+      tree = parser.compound_stmt();
+      break;
+    case 2:
+      parser = getCPPParser(chars);
+      parser.buildParseTrees = true;
+      tree = parser.compoundStatement();
+      break;
+    case 3:
+      parser = getJavaScriptParser(chars);
+      parser.buildParseTrees = true;
+      tree = parser.program();
+  }
 
-	var visitorObject = new Visitor(languageIndex);
-	tree.accept(visitorObject);
+  var visitorObject = new Visitor(languageIndex);
+  tree.accept(visitorObject);
 
-	var ruleNames = visitorObject.getRuleNames();
-	console.log("rulename", ruleNames);
+  var ruleNamesIndices = visitorObject.getRuleNamesIndices();
+  console.log("rulenameIndices", ruleNamesIndices);
+  console.log("Rule names : \n", visitorObject.getRuleNames());
+  //Define n
+  var n = 3;
 
-	//Define n
-	var n = 3;
+  //Get N-Grams
+  var nArr = getNGrams(ruleNamesIndices, n);
+  console.log(nArr);
 
-	//Get N-Grams
-	var nArr = getNGrams(ruleNames, n);
-	console.log(nArr);
+  // Encrypt using MD5 and get HashSet
+  var hashSet = getHashSet(nArr);
+  console.log("Hash Set:", hashSet);
 
-	// Encrypt using MD5 and get HashSet
-	var hashSet = getHashSet(nArr);
-	console.log("Hash Set:", hashSet);
+  // Encrypt the HashSet using SHA256 and get the Code Fingerprint
+  var codeFingerprint = getCodeFingerprint(hashSet);
+  console.log("Code Fingerprint:", codeFingerprint);
 
-	// Encrypt the HashSet using SHA256 and get the Code Fingerprint
-	var codeFingerprint = getCodeFingerprint(hashSet);
-	console.log("Code Fingerprint:", codeFingerprint);
+  return hashSet;
 }
-
-printTree(2);
+var languageIndex = 0;
+var fname = sourceCodeFileNames[languageIndex];
+const fileName = `./source_code/${fname}`;
+printTree(languageIndex, fileName);
+export { printTree };
--- Javascript/source_code/java_source_code.java
+++ Javascript/source_code/java_source_code.java
@@ -2,10 +2,11 @@ public class java_source_code {
     public static void main(String args[]) {
         int a=5; 
         // This is a comment
-
         /**
          * This is another comment
          */
         System.out.println("Testing antlr");   
     }
 }
+
+
